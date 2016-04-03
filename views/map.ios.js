f8ed833efa24122e348a04a84b3f9dc311876839
Prince Sadie <princesadie@gmail.com>
2016-04-03 16:03:21 -0500

    Add new map

--- views/map.ios.js
+++ views/map.ios.js
@@ -8,23 +8,19 @@ var {
   TouchableOpacity,
   AlertIOS,
 } = React;
-
+import Firebase from 'firebase';
 var MapView = require('react-native-maps');
-
 var { width, height } = Dimensions.get('window');
 
 const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
+const LATITUDE = 41.889357;
+const LONGITUDE = -87.637604;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 let id = 0;
 
-function randomColor() {
-  return '#'+Math.floor(Math.random()*16777215).toString(16);
-}
-
-var DefaultMarkers = React.createClass({
+var MapDisplay = React.createClass({
+  watchID: (null: ?number),
   getInitialState() {
     return {
       region: {
@@ -34,45 +30,158 @@ var DefaultMarkers = React.createClass({
         longitudeDelta: LONGITUDE_DELTA,
       },
       markers: [],
+      request: {},
+    };
+  },
+
+  updateUserLocationInFirebase() {
+    var userRef = new Firebase("https://snapdrop.firebaseio.com/users/0")
+    userRef.update({
+      lat: this.state.lastPosition.lat,
+      long: this.state.lastPosition.long,
+    })
+  },
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = {
+          long: parseFloat(position.coords.longitude),
+          lat: parseFloat(position.coords.latitude)
+        }
+        this.setState({
+          initialPosition
+        })
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 5000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = {
+        long: parseFloat(position.coords.longitude),
+        lat: parseFloat(position.coords.latitude)
+      }
+      this.setState({
+        lastPosition
+      });
+      this.updateUserLocationInFirebase();
+    });
+  },
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  },
+
+  onRegionChange(region) {
+    this.setState({ region });
+  },
+
+  jumpRandom() {
+    this.setState({ region: this.goToMarker() });
+  },
+
+  animateRandom() {
+    if(this.state.markers[0] != null) {
+      this.refs.map.animateToRegion(this.goToMarker());
+    }
+  },
+
+  goToMarker() {
+    var { markers } = this.state;
+    return {
+      ...this.state.markers[0],
+      latitude: markers[0].coordinate.latitude,
+      longitude: markers[0].coordinate.longitude,
     };
   },
 
   onMapPress(e) {
+    if(this.state.markers.length < 1) {
+      this.setState({
+        markers: [
+          ...this.state.markers,
+          {
+            coordinate: e.nativeEvent.coordinate,
+            key: id++,
+            title: 'PIN',
+            description: 'DESCRIPTION'
+          },
+        ],
+      });
+      console.log(e.nativeEvent.coordinate)
+    }
+  },
+
+  sendRequestToFireBase() {
+    var userRef = new Firebase("https://snapdrop.firebaseio.com/users/0")
+    userRef.update({
+      requestDescription: this.state.request.description,
+      requestLat: this.state.markers[0].coordinate.latitude,
+      requestLong: this.state.markers[0].coordinate.longitude
+    })
+  },
+
+  updateMarkerCoordinate(e) {
     this.setState({
       markers: [
-        ...this.state.markers,
+        ...this.state.markers[0],
         {
           coordinate: e.nativeEvent.coordinate,
-          key: id++,
-          color: randomColor(),
-          title: 'PIN',
-          description: 'DESCRIPTION'
         },
       ],
+    })
+  },
+
+  saveResponse(promptValue) {
+    this.setState({
+      request: {
+        lat: this.state.markers[0].coordinate.latitude,
+        long: this.state.markers[0].coordinate.longitude,
+        description: promptValue
+      }
     });
-    console.log(this.state.markers)
   },
 
   render() {
     return (
       <View style={styles.container}>
         <MapView
+          ref="map"
+          mapType="terrain"
           style={styles.map}
-          initialRegion={this.state.region}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
           onPress={this.onMapPress}
+          showsUserLocation={true}
         >
-          {this.state.markers.map(marker => (
+        {this.state.markers.map(marker => (
             <MapView.Marker
               key={marker.key}
               coordinate={marker.coordinate}
               pinColor={marker.color}
-            />
+              onDragEnd={this.updateMarkerCoordinate}
+              onSelect={() => AlertIOS.prompt('WRITE DESCRIPTION', null, this.saveResponse)}
+              draggable>
+                <MapView.Callout>
+                  <View>
+                    <Text>{this.state.markers[0].coordinate.latitude.toPrecision(7)},{this.state.markers[0].coordinate.longitude.toPrecision(7)}</Text>
+                  </View>
+                </MapView.Callout>
+            </MapView.Marker>
           ))}
         </MapView>
+        <View style={[styles.bubble, styles.latlng]}>
+          <Text style={{ textAlign: 'center'}}>
+            {`${this.state.region.latitude.toPrecision(7)}, ${this.state.region.longitude.toPrecision(7)}`}
+          </Text>
+        </View>
         <View style={styles.buttonContainer}>
-          <View style={styles.bubble}>
-            <Text>Tap to create a marker of random color</Text>
-          </View>
+          <TouchableOpacity onPress={this.sendRequestToFireBase} style={[styles.bubble, styles.button]}>
+            <Text>SEND</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.animateRandom} style={[styles.bubble, styles.button]}>
+            <Text>MOVE</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -119,4 +228,4 @@ var styles = StyleSheet.create({
   },
 });
 
-module.exports = DefaultMarkers;
+module.exports = MapDisplay;
